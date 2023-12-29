a91039016b3a5275c04656130e7ce7dbfba5d6cb
pedromdcr <suspectmachine@gmail.com>
2023-12-29 11:29:07 -0300

    second commit

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/explorer')
+def explorer():
+    return render_template('explorer.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
